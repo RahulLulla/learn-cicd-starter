2a281b87e339d0057d441366975846d44a3c7091
rahullulla <rahullulla20@gmail.com>
2026-04-14 09:38:04 +0530

    tests of ci

---
+++ internal/auth/get_api_key_test.go
@@ -0,0 +1,21 @@
+package auth
+
+import (
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+
+func TestGetAuthKey(t *testing.T) {
+	req, _ := http.NewRequest("GET", "https://example.com", nil)
+	wantAuthKey := "your_token_here"
+	req.Header.Set("Authorization", "ApiKey "+wantAuthKey)
+	authKey, errorMessage := GetAPIKey(req.Header)
+	if !reflect.DeepEqual(authKey, wantAuthKey){
+		t.Fatalf("Expected: %v, got: %v", wantAuthKey, authKey)
+	}
+	if !reflect.DeepEqual(errorMessage, nil){
+		t.Fatalf("Expected: %v, got: %v", nil, errorMessage)
+	}
+}
